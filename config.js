0c981a5e47af4e0e1118dac3d3ce52c482f162b9
TxExJ <118063889+TxExJ@users.noreply.github.com>
2022-11-14 09:02:52 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-central-1_JZN9VeyK9', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3ss345kbj5dnb9t5k6bpfgcbbu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
