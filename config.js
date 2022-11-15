531f972059512783b7ab98e4a6c24beb65b609a2
TxExJ <118063889+TxExJ@users.noreply.github.com>
2022-11-15 22:40:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-central-1_JZN9VeyK9', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3ss345kbj5dnb9t5k6bpfgcbbu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
